d894834a0c9eb9930130b3ece9778592477ca902
shlomi1993 <72878018+shlomi1993@users.noreply.github.com>
2022-01-12 21:06:48 +0200

    up up up dates

---
+++ submissions/ex4_submit/ex4.2_submit/12.1.22/controllers/foraging/krembot.ino.cpp
@@ -0,0 +1,2 @@
+#include "krembot.ino.h"
+
---
+++ submissions/ex4_submit/ex4.2_submit/12.1.22/controllers/foraging_1/krembot.ino.h
@@ -0,0 +1,26 @@
+// Shlomi Ben-Shushan 311408264
+// Yiftach Neuman 208305359
+
+#include <Krembot/controller/krembot_controller.h>
+#include "controllers/foraging/krembot.ino.h"
+
+/*
+ * Class:       foraging_1_controller
+ * Description: The controller of forager-1 robot.
+ */
+class foraging_1_controller : public foraging_controller {
+private:
+    enum State { spiralMove, spiralTurn, move, rtb, softTurn, hardTurn, blockNest };
+    State state;
+    SandTimer sandTimer, spiralTurnTimer, spiralStartTimer;
+    millis_time_t time_until_next_turn;
+    int8_t direction;
+    int8_t ourColor, theirColor, ourBaseColor, theirBaseColor;
+    int8_t adder;
+    bool towardOpponentNest;
+public:
+    void setup() override;
+    void loop() override;
+};
+
+REGISTER_CONTROLLER(foraging_1_controller, "foraging_1_controller")
