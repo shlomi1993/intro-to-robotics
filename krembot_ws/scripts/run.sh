ddce6bc46d31b3720bf0e0427b00f1b967c0c04c
shlomi1993 <shlomiggl@gmail.com>
2022-02-14 11:45:43 +0200

    Update run.sh

--- krembot_ws/scripts/run.sh
+++ krembot_ws/scripts/run.sh
@@ -9,16 +9,16 @@ cd ..
 echo "Run simulation…" ; echo
 unset ARGOS_PLUGIN_PATH
 
-if [ "$1" == "1" ]
+if [ "$1" == "warmup" ]
 then
 	argos3 -c ./config/ex1.argos
-elif [ "$1" == "2" ]
+elif [ "$1" == "wanderbot" ]
 then
 	argos3 -c ./config/ex2.argos
-elif [ "$1" == "3" ]
+elif [ "$1" == "navigation" ]
 then
 	argos3 -c ./config/PRM.argos
-elif [ "$1" == "4" ]
+elif [ "$1" == "foraging" ]
 then
 	if [ "$2" == "r" ]
 	then
@@ -26,20 +26,29 @@ then
 	else 
 		argos3 -c ./config/tournament.argos
 	fi
-elif [ "$1" == "all" ]
+elif [ "$1" == "mixed_controllers" ]
 then
-	argos3 -c ./config/PRM.argos
 	argos3 -c ./config/mixed_controllers.argos
+fi
+elif [ "$1" == "porting_example" ]
+then
 	argos3 -c ./config/porting_example.argos
-	argos3 -c ./config/program_template.argos
+fi
+elif [ "$1" == "run_away_by_bumpers" ]
+then
 	argos3 -c ./config/run_away_by_bumpers.argos
+fi
+elif [ "$1" == "run_away_by_bumpers_multiple" ]
+then
 	argos3 -c ./config/run_away_by_bumpers_multiple.argos
+fi
+elif [ "$1" == "sand_timer_test" ]
+then
 	argos3 -c ./config/sand_timer_test.argos
+fi
+elif [ "$1" == "simple_demo" ]
+then
 	argos3 -c ./config/simple_demo.argos
-	argos3 -c ./config/ex1.argos
-	argos3 -c ./config/ex2.argos
-	argos3 -c ./config/PRM.argos
-	argos3 -c ./config/tournament.argos
 fi
 
 echo "Done." ; echo
